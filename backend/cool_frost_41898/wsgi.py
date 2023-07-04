ae0d3746faaccdfe6f3d3db18a9099027b17e29d
Crowdbotics <team@crowdbotics.com>
2023-07-04 12:56:45 +0000

    Initial commit.

---
+++ backend/cool_frost_41898/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cool_frost_41898 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cool_frost_41898.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cool-frost-41898",
+  url: "https://cool-frost-41898.botics.co",
+  api: "https://cool-frost-41898.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
